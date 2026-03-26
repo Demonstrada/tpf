6e1b0b30558d1392b86dd25cd6dd1aa1e2f00054
david <davidpazguirao@gmail.com>
2026-03-26 21:45:23 +0100

    8

--- src/App.js
+++ src/App.js
@@ -157,7 +157,7 @@ function App() {
   const handleRequestFullscreen = useCallback((iframeContent, stats) => { setFullscreenReplay({ iframeContent, stats }); }, []);
   const handleCloseFullscreen = useCallback(() => { setFullscreenReplay(null); }, []);
 
-  const baseURL = "https://backend-enqf.onrender.com";
+  const baseURL = "https://backend-21g1.vercel.app/";
 
   useEffect(() => {
     const buttons = document.querySelectorAll(".btn");
