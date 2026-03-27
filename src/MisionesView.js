2a709f70a37f79172ec764a8ab89623cc1d8007d
david <davidpazguirao@gmail.com>
2026-03-27 17:07:01 +0100

    18

--- src/MisionesView.js
+++ src/MisionesView.js
@@ -180,7 +180,7 @@ export default function MisionesView({ baseURL, usuarioData }) {
                 paddingBottom: "8px", display: "flex", alignItems: "center", gap: "8px" 
               }}>
                 {evento === "Misiones Generales" ? "🌟" : "🎟"} {evento}
-                {evento === "Misiones Generales" ? evento : `Evento - ${evento}`}
+                {evento === "Misiones Generales" ? "" : `Evento - ${evento}`}
               </h2>
 
               {/* Acordeones de Grupos dentro de este evento */}
